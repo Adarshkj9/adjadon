7260dce92d3df2ffc91bf9349a4ea1a2fa7ee02a
Adarshkj9 <56882043+Adarshkj9@users.noreply.github.com>
2022-03-19 11:12:34 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,14 @@
+function validate()
+{
+var username=document.getElementById("username").value;
+var password=document.getElementById("password").value;
+if(username=="Adarsh"&& password=="1234")
+{
+alert("login succesfully");
+return false;
+}
+else
+{
+alert("login failed");
+}
+}
